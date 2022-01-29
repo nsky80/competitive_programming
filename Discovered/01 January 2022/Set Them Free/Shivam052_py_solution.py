013f1a7ea91cb26dbb568b66044b4985fcdfe75c
Shivam Mishra <50204246+Shivam052@users.noreply.github.com>
2022-01-30 01:01:31 +0530

    solution for the nexted list

--- Discovered/01 January 2022/Set Them Free/Shivam052_py_solution.py
+++ Discovered/01 January 2022/Set Them Free/Shivam052_py_solution.py
@@ -1,1 +1,16 @@
-#!/bin/python3
+#recursive function
+def free(lst):
+    freelst = [] # initialize empty list
+    for i in lst: # loop to identify the list or other data type
+        if type(i) == list:
+            freelst.extend(free(i)) # if list call the recursive function
+        else:
+            freelst.append(i) # append in the free list
+    return freelst # return the free list
+
+
+if __name__ == '__main__':
+    #takes the whole nexted list as input
+    nexted_lst = eval(input("Enter the whole nexted list: "))
+    #print the free list
+    print(free(nexted_lst))
